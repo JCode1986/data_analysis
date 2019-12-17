4e46c8224a21c8dae80fcb38fdb19a3220383563
Joseph Hangarter <joseph.hangarter@yahoo.com>
2019-12-16 22:03:40 -0800

    initial commit

---
+++ fix.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Script to fix symlinks for virtualenv created by pipenv 
+# symlink for lib-download directory instead of the files within
+
+pipenv_dir=$(pipenv --venv)
+python_dir=$(ls $pipenv_dir/lib | head -1)
+
+venv_lib_dir=$pipenv_dir/lib/$python_dir
+conda_dir=$(which $python_dir)
+
+
+echo "Deleting: $venv_lib_dir/lib-dynload/"
+rm -rf $venv_lib_dir/lib-dynload/
+
+echo "Creating symlink: ln -s $conda_dir/lib-dynload $venv_lib_dir/lib-dynload"
+ln -s $conda_dir/lib-dynload $venv_lib_dir/lib-dynload
+
+echo "Done"
